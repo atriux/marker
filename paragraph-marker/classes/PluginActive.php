578eb3b1028585162a287d4387c4aabdbf36dac6
Forever Sleepless <64736681+atriux@users.noreply.github.com>
2022-06-26 18:18:22 +0100

    Code copy correction

--- paragraph-marker/classes/PluginActive.php
+++ paragraph-marker/classes/PluginActive.php
@@ -1,69 +1,33 @@
 <?php
 namespace PostHighlighter;
-class PostParagraph{
-	public function __construct(){
-		add_action( 'init', [$this,'create_post_paragraph'] );// Regsiter custom post type	
-		add_action('admin_menu', [$this,'register_custom_submenu']);// Wordpress admin menu
-		add_filter( 'the_content', [$this,'add_link_to_main_post'], 1 );
-		add_filter( 'the_excerpt', [$this,'add_link_to_main_post'], 1 );
-	}
-	public function add_link_to_main_post( $content ) {
-		if ( get_post_type() == 'post-paragraph' ) {
-			$post_paragraph = get_post_meta( get_the_ID() , 'post_id' , true );
-			if( $post_paragraph && ( $post_paragraph_link = get_permalink($post_paragraph) ) )
-				return $content . " Click <a href='$post_paragraph_link' target='_blank'>here</a> to go to main post.";
-		}
-		return $content ;
-	}
-	/** Create custom post **/
-	public function create_post_paragraph(){
-		register_post_type( 'post-paragraph',
-			array(
-				'labels' => array(
-					'name' => __( 'Post Paragraph' ),
-					'singular_name' => __( 'Post Paragraph' )
-				),
-				'public' => true,
-				'has_archive' => true,
-				'show_in_rest' => true,
-				'supports'	=>	['title','editor','excerpt','thumbnail'],
-				'capability_type' => 'post',
-				  'capabilities' => array(
-				    'create_posts' => false, // Removes support for the "Add New" function ( use 'do_not_allow' instead of false for multisite set ups )
-				  ),
-				  'map_meta_cap' => true
-			)
-		);
-		register_taxonomy( 'post-paragraph-tag', array( 'post-paragraph' ) , [
-			'show_in_rest'	=>	true
-		] );
-	}
-	/** Add sub menu under custom post type **/
-	public function register_custom_submenu(){
-		/** Save by popularity **/
-		add_submenu_page(
-			'edit.php?post_type=post-paragraph',
-			'Saves by Popularity',
-			'Saves by Popularity',
-			'manage_options',
-			'post-highlighter-saves-by-popularity',
-			[$this,'saves_by_popularity']
-		);
-		/** All Saves **/
-		add_submenu_page(
-			'edit.php?post_type=post-paragraph',
-			'All Saves',
-			'All Saves',
-			'manage_options',
-			'post-highlighter-all-saves',
-			[$this,'all_saves']
-		);
-	}
-	public function saves_by_popularity(){
-		include(POST_HIGHLIGHTER_PATH . "template/saves-by-popularity.php");
-	}
-	public function all_saves(){
-		include(POST_HIGHLIGHTER_PATH . "template/all-saves.php");
+class PluginActive{
+	
+	public static function CreateTables(){
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'user_paragraphs';
+		$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		user_id BIGINT(9) NULL,
+		paragraph_post_id BIGINT(9) NOT NULL,
+		highlighted_on datetime NOT NULL,
+		cookie_id varchar(100) NULL,
+		ip_address varchar(100) NULL,
+		ip_info text NULL,
+		deleted_at datetime NULL,
+		PRIMARY KEY  (id)
+		) $charset_collate;";//
+		dbDelta( $sql );
+		$table_name = $wpdb->prefix . 'paragraphs_shared_on';
+		$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		user_paragraph_id BIGINT(9) NULL,
+		shared_on varchar(100) NOT NULL,
+		shared_at datetime NOT NULL,
+		shared_by BIGINT(9) NULL,
+		PRIMARY KEY  (id)
+		) $charset_collate;";//
+		dbDelta( $sql );
 	}
 }
-new PostParagraph;
